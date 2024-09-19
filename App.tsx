00910d77d8a2b5e34d4598fddd6d3cd2b7dec3c4
shadowAssassin23 <fighterb683@gmail.com>
2024-09-19 11:19:21 +0530

    add useState and functions

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
 //  For validation
 import * as Yup from 'yup';
 
-const PasswordSchema = Yup.object({
+const PasswordSchema = Yup.object().shape({
   PasswordLength: Yup.number()
     .min(4, 'should be min of 4 characters')
     .max(16, 'should be max of 16 character')
@@ -12,6 +12,25 @@ const PasswordSchema = Yup.object({
 });
 
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  const generatePasswordString = () => {
+    // todo
+  };
+
+  const createPassword = () => {
+    // todo
+  };
+
+  const resetPassword = () => {
+    // todo
+  };
+
   return (
     <View style={{}}>
       <Text>Hello</Text>
